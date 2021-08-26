d6914f99e56120708072c5885ee53c997e5f3641
Younes <77597800+younesAlgonquin@users.noreply.github.com>
2021-08-26 11:08:24 -0400

    Update TestSearchSort.java

--- TestSearchSort.java
+++ TestSearchSort.java
@@ -5,15 +5,10 @@ import java.util.Scanner;
 
 /**
  * CET - CS Academic Level 3
- * <br>Declaration: I declare that this is my own original work and is free from Plagiarism
  * <br>This class tests the program by displaying a menu of options and prompts users to choose one of them.
  * <br>then it displays an array of integers and performs binary & iterative search and sorts the array using the chosen algorithm 
- * <br>Student Name: Younes Boutaleb
- * <br>Student Number: 041019068 
  * <br>Section #: 303
  * <br>Course: CST8130 - Data Structures
- * <br>Professor: James Mwangi PhD.
- * @author Younes Boutaleb.
  * @version 1.0
  * 
  */
